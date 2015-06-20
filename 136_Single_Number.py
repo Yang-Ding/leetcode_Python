93dd9b5811fbca5d895a4244e4b8e61e69930b30
Yang Ding <dingyang@sas.upenn.edu>
2015-06-20 14:48:33 -0400

    136:Single Number

---
+++ 136_Single_Number.py
@@ -0,0 +1,8 @@
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer}
+    def singleNumber(self, nums):
+        x=0
+        for i in range(len(nums)):
+           x^=nums[i] 
+        return x
