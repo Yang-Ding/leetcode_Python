e5fa44ba163e8aac008ef3ab56a410026ae9f5a0
Yang Ding <dingyang@sas.upenn.edu>
2015-07-19 01:35:19 -0400

    62: Unique Paths

---
+++ 62_Unique_Paths.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param {integer} m
+    # @param {integer} n
+    # @return {integer}
+    def uniquePaths(self, m, n):
+        row=n*[0]
+        M=[row]*m
+        for i in range(m):
+            M[i][0]=1
+        for i in range(n):
+            M[0][i]=1
+        for i in range(1,m):
+            for j in range(1,n):
+                M[i][j]=M[i-1][j]+M[i][j-1]
+        return M[m-1][n-1]
